8d9bc04e656ada8307b758eaebffdb89fa48b5c0
mizan <mizzanur007@gmail.com>
2020-07-23 12:57:32 +0600

    importing npm modules and working with it

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,22 +1,13 @@
-// Challenge Time
-// creating separate notes.js file and a function which would print 'My notes to the console'
-const notesFile = require('./notes.js')
+const validator = require('validator')
 
-const notes = notesFile()
+const notes = require('./notes.js')
 
-console.log(notes)
+const message = notes()
+
+console.log(message)
 
 
-// // require('./utility.js') // it will log out first because it happening first.
-// // const lastName = require('./utility.js') // it will log out first because it happening first.
-// const addNum = require('./utility.js') // it will log out first because it happening first.
-
-// // const name = 'Mizan'
-
-// const sum = addNum(5, 4)
-
-// // console.log(lastName) // it will log out later because it happening later.
-// console.log(sum) // it will log out later because it happening later.
-
-// // modules have their own scopes. 
-// // app.js can not access variables from utility.js
+console.log(validator.isEmail('mizan@gmail.com'))
+console.log(validator.isEmail('mizan@gmail'))
+console.log(validator.isURL('github'))
+console.log(validator.isURL('https://github.com'))
