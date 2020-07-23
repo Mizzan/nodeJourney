de760cda31821318e594b736fb3b9d844274b8f0
mizan <mizzanur007@gmail.com>
2020-07-23 14:42:47 +0600

    importing the npm modules and using the npm modules through documentation

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,13 +1,16 @@
-const validator = require('validator')
+const chalk = require('chalk')
 
-const notes = require('./notes.js')
+const log = console.log;
 
-const message = notes()
+log(chalk.red.bgBlue.bold('Success'))
 
-console.log(message)
+log(chalk.keyword('orange')('Yay for orange colored text!'));
+log(chalk.rgb(123, 45, 67).underline('Underlined reddish color'));
+log(chalk.hex('#DEADED').bold('Bold gray!'));
 
 
-console.log(validator.isEmail('mizan@gmail.com'))
-console.log(validator.isEmail('mizan@gmail'))
-console.log(validator.isURL('github'))
-console.log(validator.isURL('https://github.com'))
+log(`
+CPU: ${chalk.red('90%')}
+RAM: ${chalk.green('40%')}
+DISK: ${chalk.yellow('70%')}
+`);
